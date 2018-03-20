fff86e15c266cdc6f1dbd00ab76ddd94f7f35f29
yany2 <yuan.yan@dianrong.com>
2018-03-20 14:20:29 +0800

    redux使用

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,24 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.incrementAsync = this.incrementAsync.bind(this);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-  }
-
-  incrementIfOdd() {
-    if (this.props.value % 2 !== 0) {
-      this.props.onIncrement();
-    }
-  }
-
-  incrementAsync() {
-    setTimeout(this.props.onIncrement, 1000);
-  }
 
   render() {
-    const { value, onIncrement, onDecrement, incrementAsync, addTwice } = this.props;
+    const { value, onIncrement, onDecrement } = this.props;
     console.log(this.props);
     return (
       <p>
@@ -32,14 +17,6 @@ class Counter extends Component {
         <button onClick={onDecrement}>
           -
         </button>
-        {' '}
-        <button onClick={incrementAsync}>
-          Increment async
-        </button>
-        {' '}
-        <button onClick={addTwice}>
-          +2
-        </button>
       </p>
     )
   }
@@ -49,8 +26,6 @@ Counter.propTypes = {
   value: PropTypes.number.isRequired,
   onIncrement: PropTypes.func.isRequired,
   onDecrement: PropTypes.func.isRequired,
-  incrementAsync: PropTypes.func.isRequired,
-  addTwice: PropTypes.func.isRequired,
 };
 
 export default Counter;
