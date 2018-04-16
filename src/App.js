67842920ae5679d6077d55fd541b9e68188b7478
yany2 <yuan.yan@dianrong.com>
2018-04-16 13:49:17 +0800

    part-2 update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,16 @@ import './App.css';
 
 class App extends Component {
   render() {
+    console.log('app');
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
         <div className="todoapp">
           <AddTodo/>
           <VisibleTodoList/>
